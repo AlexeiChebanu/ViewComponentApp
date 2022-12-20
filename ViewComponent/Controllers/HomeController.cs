409d9f9ba3a11954d4c5479bc2880527aa0433bf
Alexei Chebanu <0alexchebanu@gmail.com>
2022-12-20 18:11:00 +0200

    Add project files.

---
+++ ViewComponent/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ViewComponentApp.Model;
+
+namespace ViewComponentApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("about")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [Route("friends-list")]
+        public IActionResult LoadFriendsList()
+        {
+            PersonGridModel personGridModel = new PersonGridModel()
+            {
+                GridTitle = "Friends",
+                Persons = new List<Person>()
+                {
+                    new Person() {PersonName = "Alex", JobTitle = "IT"},
+                    new Person() {PersonName= "Nicole", JobTitle = "Surgeon"},
+                    new Person() {PersonName = "Sandro", JobTitle = "Sailor"}
+                }
+            };
+
+            return ViewComponent("Grid", new { grid = personGridModel });
+        }
+    }
+}
